48b582d683f82107711b79f36ce9c392b1f7fa85
dilara.kirnapci <dilara.kirnapci@kentkart.com.tr>
2020-11-28 15:34:39 +0300

    Update index file with redux configuration

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import todoReducer from './redux/reducers/todo';
+import filterReducer from './redux/reducers/filters';
+
+const rootReducer = combineReducers({
+  todoRdcr: todoReducer,
+  filterRdcr: filterReducer,
+});
+
+const logger = store => {
+  return next => {
+    return action => {
+      const result = next(action);
+      return result;
+    }
+  }
+}
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
