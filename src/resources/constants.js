8426a474cf3c29b2f436a7410f967eea7e6e0395
dilara.kirnapci <dilara.kirnapci@kentkart.com.tr>
2020-11-28 15:36:39 +0300

    Add constants file

---
+++ src/resources/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+  API_GET_TODOS: 'todos/',
+  API_POST_TODO: 'todos/create-todo',
+  API_DELETE_TODO: 'todos/delete-todo/',
+  API_UPDATE_TODO: 'todos/update-todo/',
+}
+
+export default constants;
